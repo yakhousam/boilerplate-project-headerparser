4395afe6951476e20dcebcdef06361c954f12e61
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-03-02 19:55:32 +0100

    first commit

--- server.js
+++ server.js
@@ -24,9 +24,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
